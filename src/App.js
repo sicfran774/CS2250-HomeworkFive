fbdfe3dd1d31c03be51dfc9393343814a2932262
sicfran774 <sicfran.774@gmail.com>
2023-05-03 01:07:11 -0700

    CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import BOTW from './component/botw';
 
 function App() {
   return (
-    <div className="App">
-      <BOTW></BOTW>
+    <div className="root">
+      <h1 className="header">Breath of the Wild Compendium</h1>
+      <BOTW className="botw" text="HEY"></BOTW>
     </div>
   );
 }
--- src/component/botw.jsx
+++ src/component/botw.jsx
@@ -1,65 +1,49 @@
 import React from 'react';
 
-const BOTW = () => {
-    //useState()    ===   [variable, setter]
-    const [items, setItems] = React.useState([]);
-    const [isDataLoaded, setIsDataLoaded] = React.useState(false);
-  
-    React.useEffect(() => {
-      fetch("https://botw-compendium.herokuapp.com/api/v2/all")
-        .then((res) => res.json())
-        .then((json) => {
-          setItems(json);
-          setIsDataLoaded(true);
-        });
-    }, []);
-  
-    if (!isDataLoaded) {
-      return (
-        <div>
-          <h1> Loading... </h1>
-        </div>
-      );
-    }
-    
-    const food = items.data.creatures.food;
-    const nonFood = items.data.creatures.non_food;
-    const monster = items.data.monsters;
-  
+function BOTW(text) {
+  //useState()    ===   [variable, setter]
+  const [items, setItems] = React.useState([]);
+  const [isDataLoaded, setIsDataLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://botw-compendium.herokuapp.com/api/v2/all")
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setIsDataLoaded(true);
+      });
+  }, []);
+
+  if (!isDataLoaded) {
     return (
-      <div className="App">
-        <h1> Breath of the Wild Compendium </h1>
-        {food.map((item) => (
-          <ul key={item.id}>
-            <li>
-            <img src={item.image} alt="" />
-              Name: {item.name}
-              <br></br> Description: {item.description}
-            </li>
-          </ul>
-        ))}
-        <br />
-        {nonFood.map((item) => (
-          <ul key={item.id}>
-            <li>
-            <img src={item.image} alt="" />
-              Name: {item.name}
-              <br></br> Description: {item.description}
-            </li>
-          </ul>
-        ))}
-        <br />
-        {monster.map((item) => (
-          <ul key={item.id}>
-            <li>
-            <img src={item.image} alt="" />
-              Name: {item.name}
-              <br></br> Description: {item.description}
-            </li>
-          </ul>
-        ))}
+      <div>
+        <h1> Loading... </h1>
       </div>
     );
-  };
+  }
+    
+  const food = items.data.creatures.food;
+  const nonFood = items.data.creatures.non_food;
+  const monster = items.data.monsters;
   
-  export default BOTW;
+  
+  return (
+    <div className="item-flexbox">
+      {food.map((item) => (
+        <div className="item-cell" key={item.id} >
+          <img src={item.image} alt="" />
+            <h3 className="item-name">{item.name}</h3>
+            <div className="item-cell-info">
+              <h2>{item.name}</h2>
+              <p className="item-description">{item.description}</p>
+              <p className="item-found">Found in {item.common_locations}</p>
+            </div>
+            <div className="footer"></div>
+        </div>
+      ))}
+      
+    </div>
+  );
+};
+  
+export default BOTW;
